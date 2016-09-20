774d099f7b590f215a33bda9696189f36615c0e8
bo.chen <wavecheng@gmail.com>
2016-09-20 23:01:17 +0800

    add cutsom security config & classes

---
+++ src/main/java/com/hellokoding/security/BasicUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.hellokoding.security;
+import java.util.Collections;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.identity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class BasicUserDetailsService implements UserDetailsService {
+
+  @Autowired
+  protected IdentityService identityService;
+  
+  @Override
+  public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+    User user = identityService.createUserQuery().userId(userId).singleResult();
+    if(user == null) throw new UsernameNotFoundException(userId + " not found");
+    
+    @SuppressWarnings("unchecked")
+    org.springframework.security.core.userdetails.User springUser = 
+        new org.springframework.security.core.userdetails.User(userId, user.getPassword(), Collections.EMPTY_LIST);
+    return springUser;
+  }
+
+}
---
+++ src/main/java/com/hellokoding/security/DefaultAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package com.hellokoding.security;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DefaultAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+  @Autowired
+  protected ObjectMapper objectMapper;
+  
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException authException) throws IOException, ServletException {
+    response.setContentType("application/json");
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.getWriter().print("{\"error\": "+objectMapper.writeValueAsString(authException.getMessage())+"}");
+  }
+
+}
